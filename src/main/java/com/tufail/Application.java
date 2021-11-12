5c0e5ed092d5e85598033b19cd8c6dfe9a2533d4
Tufal001 <tufail.khan56@gmail.com>
2021-11-12 15:40:35 +0530

    added one variable

--- src/main/java/com/tufail/Application.java
+++ src/main/java/com/tufail/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+	int b=200;
 	public static void main(String[] args) {
 		int i=10;
 		SpringApplication.run(Application.class, args);
